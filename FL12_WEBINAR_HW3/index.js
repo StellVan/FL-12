e9b3a8c311b6759c62e19e080971f5d0202cfc94
StellVan <balan.marian2000@gmail.com>
2020-02-23 19:53:48 +0200

    task 1 finished

--- FL12_WEBINAR_HW3/index.js
+++ FL12_WEBINAR_HW3/index.js
@@ -1,16 +1,78 @@
 'use strict';
 
+let suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+let faceCards = { 1: 'Ace', 11: 'Jack', 12: 'Queen', 13: 'King' };
+const _suit = Symbol('suit');
+const _rank = Symbol('rank');
+const _isFaceCard = Symbol('isFaceCard');
+const _wins = Symbol('wins');
+
+class Card {
+  constructor(rank, suit) {
+    this[_rank] = rank;
+    this[_suit] = suit;
+    this[_isFaceCard] = this.getFaceCard();
+  }
+
+  get rank() {
+    return this[_rank];
+  }
+
+  getFaceCard() {
+    if (Object.keys(faceCards).includes(this[_rank].toString())) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  toString() {
+    let result, valueString, suitString;
+    if (faceCards[this[_rank]]) {
+      valueString = faceCards[this[_rank]];
+    } else {
+      valueString = this[_rank];
+    }
+
+    result = `${valueString} of ${this[_suit]}`;
+    return result;
+  }
+
+  static Compare(cardOne, cardTwo) {
+    if (cardOne === 1) {
+      cardOne = 14;
+    }
+    if (cardTwo === 1) {
+      cardTwo = 14;
+    }
+    if (cardOne > cardTwo) {
+      return 'first';
+    } else if (cardOne < cardTwo) {
+      return 'second';
+    } else if (cardOne === cardTwo) {
+      return 'draw';
+    }
+  }
+}
+
 class Deck {
   constructor() {
-    this.cards = [];
-    let count = this.cards.length;
-    this.getCount = () => {
-      return count;
-    };
-    this.changeCount = newCount => {
-      count = newCount;
-    };
+    this.cards = this.createDeck();
   }
+
+  get count() {
+    return this.cards.length;
+  }
+
+  createDeck = () => {
+    let arr = [];
+    for (let i = 0; i < 4; i++) {
+      for (let j = 1; j <= 13; j++) {
+        arr.push(new Card(j, suits[i]));
+      }
+    }
+    return arr;
+  };
 
   shuffle() {
     let j, temp;
@@ -26,96 +88,58 @@ class Deck {
   }
 }
 
-class Card {
-  constructor(rank, suit) {
-    this.suit = suit;
-    this.rank = rank;
-    this.isFaceCard = 'Card.getFaceCard();';
+class Player {
+  constructor(name) {
+    this.name = name;
+    this[_wins];
+    this.deck = new Deck();
   }
 
-  get isFaceCardd() {
-    return this.isFaceCard;
+  refreshDeck() {
+    this.deck = new Deck();
   }
 
-  set isFaceCard(n) {
-    return n;
-  }
-
-  static getFaceCard() {
-    let result;
-    if (this.rank === 1 || this.rank > 10) {
-      result = true;
-    } else {
-      result = false;
+  static Play(playerOne, playerTwo) {
+    let name;
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+    playerOne.deck.shuffle();
+    playerTwo.deck.shuffle();
+    for (let i = 51; i >= 0; i--) {
+      let tempResult = Card.Compare(
+        playerOne.deck.draw(1),
+        playerTwo.deck.draw(1)
+      );
+      switch (tempResult) {
+        case 'first':
+          playerOneScore++;
+          break;
+        case 'second':
+          playerTwoScore++;
+          break;
+        case 'draw':
+          break;
+        default:
+          break;
+      }
     }
-    return result;
-  }
-
-  toString() {
-    let result, valueString, suitString;
-    switch (this.rank) {
-      case 1:
-        valueString = 'Ace';
-        break;
-      case 11:
-        valueString = 'Jack';
-        break;
-      case 12:
-        valueString = 'Queen';
-        break;
-      case 13:
-        valueString = 'King';
-        break;
-      default:
-        valueString = this.rank;
-        break;
+    if (playerOneScore > playerTwoScore) {
+      name = `${playerOne.name} Wins`;
+    } else if (playerOneScore < playerTwoScore) {
+      name = `${playerTwo.name} Wins`;
+    } else if (playerOneScore === playerTwoScore) {
+      name = 'Draw';
     }
-    switch (this.suit) {
-      case 1:
-        suitString = 'Hearts';
-        break;
-      case 2:
-        suitString = 'Diamonds';
-        break;
-      case 3:
-        suitString = 'Clubs';
-        break;
-      case 4:
-        suitString = 'Spades';
-        break;
-    }
-    result = `${valueString} of ${suitString}`;
-    return result;
+    playerOne.refreshDeck();
+    playerTwo.refreshDeck();
+    return `${name}! Score is ${playerOneScore} : ${playerTwoScore}`;
   }
-
-  Compare() {}
 }
 
-let aaa = new Card(4, 4);
+let Varvara = new Player('Varvara');
+let StellVan = new Player('StellVan');
 
-console.log(aaa.isFaceCardd);
-// this.createDeck = () => {
-//   let arr = [];
-//   let suit;
-//   for (let i = 1; i <= 4; i++) {
-//     switch (i) {
-//       case 1:
-//         suit = 'hearts';
-//         break;
-//       case 2:
-//         suit = 'diamonds';
-//         break;
-//       case 3:
-//         suit = 'clubs';
-//         break;
-//       case 4:
-//         suit = 'spades';
-//         break;
-//     }
-//     for (let j = 1; j <= 13; j++) {
-//       arr.push({ value: j, suit: suit });
-//     }
-//   }
-//   return arr;
-// };
-// this.cards = this.createDeck();
+console.log(Player.Play(Varvara, StellVan));
+console.log(Player.Play(Varvara, StellVan));
+console.log(StellVan.deck.cards[0].toString());
+console.log(StellVan.deck.cards[0].getFaceCard());
