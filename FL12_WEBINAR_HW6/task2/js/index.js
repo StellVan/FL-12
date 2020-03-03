5ee20356e080a19412e667b2945d48b98b74501d
StellVan <balan.marian2000@gmail.com>
2020-03-03 16:53:31 +0200

    fin

--- FL12_WEBINAR_HW6/task2/js/index.js
+++ FL12_WEBINAR_HW6/task2/js/index.js
@@ -1,14 +1,111 @@
-const $list = $(".list");
-const $input = $("#add-input");
-const $add = $("#add-submit");
+const $list = $('.list');
+const $input = $('#add-input');
+const $add = $('#add-submit');
 
 const todos = [
   {
-    text: "Buy milk",
+    text: 'Buy milk',
     done: false
   },
   {
-    text: "Play with dog",
+    text: 'Play with dog',
     done: true
   }
 ];
+
+$(document).ready(() => {
+  if (!getLocalStorage()) {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  refreshTodos();
+});
+
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem('todos'));
+}
+
+function refreshTodos() {
+  let listArray = getLocalStorage();
+
+  if (listArray && listArray.length > 0) {
+    $list.children().remove();
+
+    listArray.forEach(el => {
+      $list.prepend(addListItem(el.text, el.done));
+    });
+
+    addButtonsEvent();
+  }
+}
+
+$add.click(function(event) {
+  event.preventDefault();
+  let value = $input.val();
+
+  if (value.length > 0) {
+    let listArray = getLocalStorage();
+    listArray.push({ text: value, done: false });
+    localStorage.setItem('todos', JSON.stringify(listArray));
+    $input.val('');
+
+    refreshTodos();
+  } else {
+    alert('Cannot add empty ToDo!');
+  }
+});
+
+function addListItem(text, done) {
+  let classDone;
+  if (done === true) {
+    classDone = 'done';
+  } else if (done === false) {
+    classDone = '';
+  }
+  return `
+  <li class="item">
+    <span class="item-text ${classDone}">${text}</span>
+    <div class='buttonWrapper'>
+      <button class="item-remove">Remove</button>
+      <button class="item-done">Done</button>
+    </div>
+  </li>
+  `;
+}
+
+function addButtonsEvent() {
+  let $remove = $('.item-remove');
+  let $done = $('.item-done');
+
+  $remove.click(function() {
+    let id = $remove.index(this);
+    let listArray = getLocalStorage();
+
+    listArray.splice(-id - 1, 1);
+    localStorage.setItem('todos', JSON.stringify(listArray));
+
+    $remove
+      .eq(id)
+      .parent('.buttonWrapper')
+      .parent('.item')
+      .remove();
+
+    refreshTodos();
+  });
+
+  $done.click(function() {
+    let id = $done.index(this);
+    let listArray = getLocalStorage();
+
+    listArray[listArray.length - id - 1].done = true;
+    localStorage.setItem('todos', JSON.stringify(listArray));
+
+    $done
+      .eq(id)
+      .parent('.buttonWrapper')
+      .siblings('.item-text')
+      .addClass('done');
+
+    refreshTodos();
+  });
+}
